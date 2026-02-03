5a2b7294dbc1d7c2f548afabbeeae7f8d4655a23
ellee12 <dmelo9984@gmail.com>
2026-02-03 10:24:27 +0800

    added layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <v-btn>Submit</v-btn>
+    <v-row>
+      <v-col cols="12" md="3">
+        <v-card height="60" color="primary"></v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="60" color="pink accent-2"></v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="60" color="pink"></v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="60" color="purple"></v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
