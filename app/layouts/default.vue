9b01dd1bfe94ce02719ba55cf2a5f8a9d8ca6d67
Ellee <dmelo9984@gmail.com>
2026-03-17 10:34:37 +0800

    Added my project

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -16,6 +16,8 @@
           <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
           <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
           <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-group-outline" title="Prelim" value="Prelim" to="/prelim"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-group-outline" title="Remedial" value="Remedial" to="/remedial"></v-list-item>
         </v-list>
       </v-navigation-drawer>
   
---
+++ app/pages/prelim.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="24" md="12">
+        <v-card height="300" elevation="0" color="grey">
+          <v-card-text>
+            <v-row align="center" justify="center">
+              <v-col>
+                <v-row no-gutters align="center">
+                  <v-col cols="12" class="text-right text-h3 font-weight-bold">A</v-col>
+                  <v-col cols="12" class="text-right text-h6">10 points</v-col>
+                </v-row>
+              </v-col>
+              </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    
+      
+      <v-col cols="12" md="9">
+        <v-card height="300" elevation="0" color="grey">
+          <v-card-text>
+            <v-row align="center" justify="center">
+              <v-col>
+                <v-row no-gutters align="center"> 
+                  <v-col cols="12" class="text-right text-h3 font-weight-bold">B</v-col>
+                    <v-col cols="12" class="text-right text-h6">10 points</v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" md="3">
+        <v-card height="250" elevation="0" color="grey">
+          <v-card-text>
+            <v-row align="center" justify="center">
+              <v-col>
+                <v-row no-gutters align="center">
+                  <v-col cols="12" class="text-right text-h3 font-weight-bold">F</v-col>
+                  <v-col cols="12" class="text-right text-h6">10 points</v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      
+
+      
+      <v-col cols="12" md="3">
+        <v-card height="200" elevation="0" color="grey">
+          <v-card-text>
+            <v-row align="center" justify="center">
+              <v-col>
+                <v-row no-gutters align="center">
+                  <v-col cols="12" class="text-right text-h3 font-weight-bold">C</v-col>
+                  <v-col cols="12" class="text-right text-h6">10 points</v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      
+      <v-col cols="12" md="3">
+        <v-card height="200" elevation="0" color="grey">
+          <v-card-text>
+            <v-row align="center" justify="center">
+              <v-col>
+                <v-row no-gutters align="center">
+                  <v-col cols="12" class="text-right text-h3 font-weight-bold">D</v-col>
+                  <v-col cols="12" class="text-right text-h6">10 points</v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      
+      <v-col cols="12" md="3">
+        <v-card height="200" elevation="0" color="grey">
+          <v-card-text>
+            <v-row align="center" justify="center">
+              <v-col>
+                <v-row no-gutters align="center">
+                  <v-col cols="12" class="text-right text-h3 font-weight-bold">E</v-col>
+                  <v-col cols="12" class="text-right text-h6">10 points</v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      </v-row>
+   
+      
+    
+    
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
